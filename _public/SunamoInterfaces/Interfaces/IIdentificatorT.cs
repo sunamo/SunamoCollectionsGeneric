987053d44d578855846bbab451ee2c9bbe331214
sunamo.cz <radek.jancik@sunamo.cz>
2025-03-16 14:29:58 +0100

    Changes due to creating SunamoWpf

--- _public/SunamoInterfaces/Interfaces/IIdentificatorT.cs
+++
@@ -1,8 +0,0 @@
-namespace SunamoCollectionsGeneric._public.SunamoInterfaces.Interfaces;
-
-public interface IIdentificatorT<T>
-{
-    T Id { get; set; }
-    bool IsChecked { get; set; }
-    bool IsSelected { get; set; }
-}
